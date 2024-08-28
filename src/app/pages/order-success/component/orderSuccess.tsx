64d80eb3a1a8a256b1c167483f27bc3884b86b14
ChikaDibia <ogbuchika71@gmail.com>
2024-08-28 23:07:46 +0100

    Fix bugs

---
+++ src/app/pages/order-success/component/orderSuccess.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+const OrderSuccess = () => {
+  const searchParams = useSearchParams();
+  const orderId = searchParams.get("orderId") || "N/A";
+  const trackingNumber = searchParams.get("trackingNumber") || "N/A";
+
+  return (
+    <div className="bg-[#fafafa] w-[380px] h-[400px] flex flex-col justify-center items-center gap-[30px] rounded-md shadow-xl hover:shadow-2xl">
+        <h1 className="text-[22px] text-[#252B42] leading-[32px] font-bold">
+          Order Successful!
+        </h1>
+
+        <p className="text-[13px] text-[#404040] leading-[16px] font-[500] text-center">
+          Thank you for your purchase. Your order has been placed successfully.
+        </p>
+
+        <p className="text-[13px] text-[#404040] leading-[16px] font-[500]">
+          <strong>Order ID:</strong> {orderId}
+        </p>
+
+        <p className="text-[13px] text-[#404040] leading-[16px] font-[500]">
+          <strong>Tracking Number:</strong> {trackingNumber}
+        </p>
+
+        <Link
+          href="/"
+          className="w-[350px] h-[56px] flex items-center justify-center bg-[#23A6F0] rounded-[5px] text-[#ffffff] text-[20px] leading-6 font-bold"
+        >
+          Go back to home
+        </Link>
+      </div>
+  )
+}
+
+export default OrderSuccess;
--- src/app/pages/order-success/page.tsx
+++ src/app/pages/order-success/page.tsx
@@ -1,42 +1,17 @@
-// pages/order-success.tsx
-"use client";
-import React from "react";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
+import OrderSuccess from "./component/orderSuccess";
 
-const OrderSuccess = () => {
-  const searchParams = useSearchParams();
-  const orderId = searchParams.get("orderId") || "N/A";
-  const trackingNumber = searchParams.get("trackingNumber") || "N/A";
+
+
+const OrderSuccessPage = () => {
 
   return (
     <section className="w-[100%] flex justify-center items-center py-[24px]">
-      <div className="bg-[#fafafa] w-[380px] h-[400px] flex flex-col justify-center items-center gap-[30px] rounded-md shadow-xl hover:shadow-2xl">
-        <h1 className="text-[22px] text-[#252B42] leading-[32px] font-bold">
-          Order Successful!
-        </h1>
-
-        <p className="text-[13px] text-[#404040] leading-[16px] font-[500] text-center">
-          Thank you for your purchase. Your order has been placed successfully.
-        </p>
-
-        <p className="text-[13px] text-[#404040] leading-[16px] font-[500]">
-          <strong>Order ID:</strong> {orderId}
-        </p>
-
-        <p className="text-[13px] text-[#404040] leading-[16px] font-[500]">
-          <strong>Tracking Number:</strong> {trackingNumber}
-        </p>
-
-        <Link
-          href="/"
-          className="w-[350px] h-[56px] flex items-center justify-center bg-[#23A6F0] rounded-[5px] text-[#ffffff] text-[20px] leading-6 font-bold"
-        >
-          Go back to home
-        </Link>
-      </div>
+      <Suspense fallback={<div>Loading order details...</div>}>
+        <OrderSuccess />
+      </Suspense>
     </section>
   );
 };
 
-export default OrderSuccess;
+export default OrderSuccessPage;
